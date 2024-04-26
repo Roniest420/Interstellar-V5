6e8e8cbe1cfdf4146a8ff16964b0b3540488f99e
Roniest420 <161883794+Roniest420@users.noreply.github.com>
2024-04-26 12:10:37 -0500

    Update window.js

--- static/scripts/window.js
+++ static/scripts/window.js
@@ -10,7 +10,7 @@ try {
 if (!inFrame && !navigator.userAgent.includes("Firefox")) {
     const popup = open("about:blank", "_blank")
     if (!popup || popup.closed) {
-        alert("Please allow popups and redirects.")
+        alert("WMS ON TOP ?!!? v0id back .")
     } else {
         const doc = popup.document
         const iframe = doc.createElement("iframe")
